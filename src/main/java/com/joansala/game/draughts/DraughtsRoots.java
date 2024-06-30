7e8aa1694333922dbd1f3d1a65baeca5770d3a33
Joan Sala <contact@joansala.com>
2024-06-30 17:26:10 +0200

    Update and improve documentation

--- src/main/java/com/joansala/game/draughts/DraughtsRoots.java
+++ src/main/java/com/joansala/game/draughts/DraughtsRoots.java
@@ -1,7 +1,7 @@
 package com.joansala.game.draughts;
 
 /*
- * Copyright (C) 2022 Joan Sala Soler <contact@joansala.com>
+ * Copyright (C) 2022-2024 Joan Sala Soler <contact@joansala.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
