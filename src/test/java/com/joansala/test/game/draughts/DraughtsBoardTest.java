82c75feb3d99108b82c6c049d5f035f3a344001f
Joan Sala <contact@joansala.com>
2025-08-18 17:56:15 +0200

    Fix benchmark suite path for tests

--- src/test/java/com/joansala/test/game/draughts/DraughtsBoardTest.java
+++ src/test/java/com/joansala/test/game/draughts/DraughtsBoardTest.java
@@ -13,7 +13,7 @@ import com.joansala.util.suites.SuiteReader;
 public class DraughtsBoardTest implements BoardContract {
 
     /** Test suite file path */
-    private static String SUITE_PATH = "draughts-bench.suite";
+    private static String SUITE_PATH = "src/test/resources/draughts-bench.suite";
 
 
     /**
